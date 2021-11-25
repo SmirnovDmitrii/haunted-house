a5cdb6e59e29c069a5722e57845ccc3c71354e85
SmirnovDmitrii <smirnovdmitriis@ya.ru>
2021-11-25 10:24:56 +0400

    apply door textures

--- src/components/house/Door.jsx
+++ src/components/house/Door.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
 import { useLoader } from "@react-three/fiber";
-import { TextureLoader } from "three";
+import { TextureLoader, Float32BufferAttribute } from "three";
 
 const Door = (props) => {
   const [
@@ -18,13 +18,34 @@ const Door = (props) => {
     "/textures/door/height.jpg",
     "/textures/door/normal.jpg",
     "/textures/door/metalness.jpg",
-    "/textures/door/roughness.jpg"
+    "/textures/door/roughness.jpg",
   ]);
+
+  const ref = React.useRef();
+
+  React.useLayoutEffect(() => {
+    if (ref.current) {
+      ref.current.setAttribute(
+        "uv2",
+        new Float32BufferAttribute(ref.current.attributes.uv.array, 2)
+      );
+    }
+  }, []);
 
   return (
     <mesh {...props}>
-      <planeBufferGeometry args={[2, 2]} />
-      <meshStandardMaterial color={"#aa7b7b"} />
+      <planeBufferGeometry args={[2.2, 2.2, 100, 100]} ref={ref}/>
+      <meshStandardMaterial
+        map={doorColorTexture}
+        transparent
+        alphaMap={doorAlphaTexture}
+        aoMap={doorAmbientOcclusionTexture}
+        displacementMap={doorHeightTexture}
+        displacementScale={0.1}
+        normalMap={doorNormalTexture}
+        metalnessMap={doorMetalnessTexture}
+        roughness={doorRoughnessTexture}
+      />
     </mesh>
   );
 };
