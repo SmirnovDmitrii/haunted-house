df484974870a6f1450da854e753449d38e6dbc32
SmirnovDmitrii <smirnovdmitriis@ya.ru>
2021-12-02 09:52:36 +0400

    apply walls textures

--- src/components/house/Walls.jsx
+++ src/components/house/Walls.jsx
@@ -1,10 +1,39 @@
 import React from "react";
+import { useLoader } from "@react-three/fiber";
+import { Float32BufferAttribute, TextureLoader } from "three";
 
 const Walls = (props) => {
+  const [
+    bricksColorTexture,
+    bricksAmbientOcclusionTexture,
+    bricksNormalTexture,
+    bricksRoughnessTexture,
+  ] = useLoader(TextureLoader, [
+    "/textures/bricks/color.jpg",
+    "/textures/bricks/ambientOcclusion.jpg",
+    "/textures/bricks/normal.jpg",
+    "/textures/bricks/roughness.jpg",
+  ]);
+
+  const ref = React.useRef();
+
+  React.useLayoutEffect(() => {
+    if (ref.current) {
+      ref.current.setAttribute(
+        "uv2",
+        new Float32BufferAttribute(ref.current.attributes.uv.array, 2)
+      );
+    }
+  }, []);
   return (
     <mesh {...props}>
-      <boxBufferGeometry args={[4, 2.5, 4]} />
-      <meshStandardMaterial color={"#ac8e82"} />
+      <boxBufferGeometry args={[4, 2.5, 4]} ref={ref} />
+      <meshStandardMaterial
+        map={bricksColorTexture}
+        oaMap={bricksAmbientOcclusionTexture}
+        normalMap={bricksNormalTexture}
+        roughness={bricksRoughnessTexture}
+      />
     </mesh>
   );
 };
